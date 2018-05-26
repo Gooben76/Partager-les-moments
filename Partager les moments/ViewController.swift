375f266ec0ccb3985743b911129b73d79ce06979
Benoît Goossens <benoitgoossens@macben.lan>
2018-05-26 18:08:41 +0200

    Initial Commit

---
+++ Partager les moments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Partager les moments
+//
+//  Created by Benoît Goossens on 26/05/18.
+//  Copyright © 2018 Benoît Goossens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
