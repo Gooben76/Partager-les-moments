9c997bb701734da784ec2dc0c783b58f9d250935
Benoît Goossens <benoitgoossens@macben.lan>
2018-05-26 19:16:50 +0200

    Ajout de l'imagePicker et du UIAlertController

--- Partager les moments/PhotoController.swift
+++ Partager les moments/PhotoController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class PhotoController: UIViewController {
+class PhotoController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var partagerButton: UIBarButtonItem!
     @IBOutlet weak var photoAPartager: UIImageView!
     @IBOutlet weak var textAPartager: UITextView!
     
     let texteParatger = "Entrez votre texte ..."
+    var imagePicker: UIImagePickerController?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,10 +31,54 @@ class PhotoController: UIViewController {
         photoAPartager.addGestureRecognizer(tap)
         
         textAPartager.text = texteParatger
+        
+        imagePicker = UIImagePickerController()
+        imagePicker?.delegate = self
+        imagePicker?.allowsEditing = true
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        imagePicker?.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        var image:UIImage?
+        if let editee = info[UIImagePickerControllerEditedImage] as? UIImage {
+            image = editee
+        } else if let originale = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            image = originale
+        }
+        photoAPartager.image = image
+        imagePicker?.dismiss(animated: true, completion: nil)
     }
     
     @objc func cliquerPhoto(){
+        guard imagePicker != nil else {return}
+        let alert = UIAlertController(title: "Prendre une photo", message: "Sélection du média", preferredStyle: .actionSheet)
+        let appareil = UIAlertAction(title: "Appareil photo", style: .default) { (act) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                self.imagePicker?.sourceType = .camera
+                self.present(self.imagePicker!, animated: true, completion: nil)
+            }
+        }
+        let librairie = UIAlertAction(title: "Bibliothèque", style: .default) { (act) in
+            self.imagePicker?.sourceType = .photoLibrary
+            self.present(self.imagePicker!, animated: true, completion: nil)
+        }
+        let annuler = UIAlertAction(title: "Annuler", style: .cancel, handler: nil)
+        alert.addAction(appareil)
+        alert.addAction(librairie)
+        alert.addAction(annuler)
         
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            if let pop = popoverPresentationController{
+                pop.sourceView = self.view
+                pop.sourceRect = CGRect(x: self.view.frame.midX, y: self.view.frame.midY, width: 0, height: 0)
+                pop.permittedArrowDirections = []
+                
+            }
+        }
+        self.present(alert, animated: true, completion: nil)
     }
     
     @IBAction func partagerClick(_ sender: Any) {
